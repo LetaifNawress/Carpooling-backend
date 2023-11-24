8624f1f6648ecdd7a4c9e8d58818e24d435bcda1
Naimatia <119688341+Naimatia@users.noreply.github.com>
2023-11-24 14:36:15 +0100

    Delete src directory

--- src/main/java/com/example/projetetudiant/security/repository/appRoleRep.java
+++
@@ -1,8 +0,0 @@
-package com.example.projetetudiant.security.repository;
-
-import com.example.projetetudiant.security.entities.appRole;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface appRoleRep extends JpaRepository<appRole,Long> {
-    appRole findByRolename(String rolename);
-}
